d60cb0a82446303f74b49777740d77a0ab2831fa
Nguyen Ba Luan <61571929+dongcuavodoi@users.noreply.github.com>
2022-02-20 22:55:47 +0700

    Update Task5

--- Task4/src/App.vue
+++ Task4/src/App.vue
@@ -1,20 +1,81 @@
 <template>
   <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hello World"></HelloWorld>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- v-if -->
+    <button @click="awesome = !awesome">Toggle</button>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+    <!-- v-for -->
+    <ul>
+      <li v-bind:key="id" v-for="(value, id, index) in myObject">
+        {{ index }}. {{ id }}: {{ value }}
+      </li>
+    </ul>
+    <form v-on:submit.prevent="addNewTodo">
+      <label for="new-todo">Add a todo</label>
+      <input
+        v-model="newTodoText"
+        id="new-todo"
+        placeholder="E.g. Feed the cat"
+      />
+      <button>Add</button>
+    </form>
+    <ul>
+      <todo-item
+        v-for="(todo, index) in todos"
+        :key="todo.id"
+        :title="todo.title"
+        @remove="todos.splice(index, 1)"
+      ></todo-item>
+    </ul>
   </div>
 </template>
 
 <script>
-// import ButtonCounter from './components/ButtonCounter.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-  }
-}
+    TodoItem,
+  },
+  data() {
+    return {
+      awesome: true,
+      myObject: {
+        title: "How to do lists in Vue",
+        author: "Anonymous author ",
+        publishedAt: "2016-04-10 ",
+      },
+      newTodoText: "",
+      todos: [
+        {
+          id: 1,
+          title: "Do the dishes",
+        },
+        {
+          id: 2,
+          title: "Take out the trash",
+        },
+        {
+          id: 3,
+          title: "Mow the lawn",
+        },
+      ],
+      nextTodoId: 4,
+    };
+  },
+  methods: {
+    addNewTodo() {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText,
+      });
+      this.newTodoText = "";
+    },
+  },
+};
 </script>
 
 <style>
